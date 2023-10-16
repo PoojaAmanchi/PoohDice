343ff2f5048c6a3f5a8abaac6d814ce983689a9d
POOJA AMANCHI <140485017+PoojaAmanchi@users.noreply.github.com>
2023-10-16 20:59:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomImage1 = "images/dice"+randomNumber1+".png";
+var randomImage1 = "dice"+randomNumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomImage1)
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImage2 = "images/dice"+randomNumber2+".png";
+var randomImage2 = "dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2)
 
 if(randomNumber1>randomNumber2)
